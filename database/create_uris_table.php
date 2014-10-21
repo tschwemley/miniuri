2428617c6f8884f7a778bcc203e2911ab82737a5
Tyler <tyler@medhub.com>
2014-10-21 15:17:49 -0400

    Added create uri table script

--- database/create_uris_table.php
+++ database/create_uris_table.php
@@ -4,11 +4,11 @@ require_once 'db.php';
 
 $sql = 'CREATE TABLE IF NOT EXISTS `uris` (
 			`id` int(11) NOT NULL auto_increment,
-			`uri_link` varchar(255) default NULL,
-			`uri_short` varchar(6) default NULL,
-			`uri_date` int(10) default NULL,
-			`uri_ip` varchar(255) default NULL,
-			`uri_hits` int(11) default 0,
+			`uri` varchar(255) default NULL,
+			`short_key` varchar(6) default NULL,
+			`date` int(10) default NULL,
+			`ip` varchar(255) default NULL,
+			`hits` int(11) default 0,
 			PRIMARY_KEY (`id`)
 		) ENGINE=InnoDB DEFAULT CHARSET=latin1';
 
