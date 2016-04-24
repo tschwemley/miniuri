7b2472f185a761f2c6dbfca6531cc84ade464d15
Tyler Schwemley <tjschwem@gmail.com>
2016-04-24 13:56:45 -0400

    Generating uris properly using ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,28 @@
+'use strict';
+
 $(function() {
-  var minButton = $('.min-button');
-  var uri = $('.url').text();
-  alert(uri);
-  minButton.on('click', function() {
+  var uri;
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    uri = $('.url').val();
+    
     $.ajax({
-      url: 'https://0t1shqh0wj.execute-api.us-east-1.amazonaws.com/Production/create-uri',
-      method: 'PUT',
-      data: {
-        uri: uri
-      },
+      type: 'PUT',
       dataType: 'json',
+      url: "https://0t1shqh0wj.execute-api.us-east-1.amazonaws.com/Production/create-uri",
+      data: "{\"uri\": \"" + uri + "\"}"
     })
     .fail(function(jqXHR, textStatus, error) {
-      console.log("jqXHR");
-      console.log(jqXHR);
-      console.log("status: " + textStatus);
       console.log("error: " + error);
     })
     .success(function(data, textStatus) {
-      console.log("success: " + data);
+      console.log(data);
+      var miniUri = "http://miniuri.me/" + data.short_key;
+      var successText = "Success! Your minimized uri is: <a href='" + miniUri 
+        + "'>" + miniUri;
+
+      $('.uri-display').html(successText).show();
     });
   });
 });
